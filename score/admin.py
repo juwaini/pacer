2bb9f14a02ce32cd15af5fe49a1a1ec427b42048
Juwaini <juwaini@gmail.com>
2023-01-25 16:13:43 +0800

    refactor

--- score/admin.py
+++ score/admin.py
@@ -5,7 +5,7 @@ from score.models import Score
 
 class ScoreAdmin(admin.ModelAdmin):
     model = Score
-    # list_display = '__all__'
+    list_display = 'user', 'score',  'last_check'
 
 
 admin.site.register(Score, ScoreAdmin)
--- score/tests.py
+++ score/tests.py
@@ -1,3 +1,24 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from score.models import Score
+
+
+class ScoreTest(TestCase):
+    def setUp(self) -> None:
+        self.u1 = User.objects.create(username='usertest1')
+        self.u2 = User.objects.create(username='usertest2')
+
+    def test_correct_score(self):
+        Score.objects.create(user=self.u1, score=10, last_check=datetime.datetime.now())
+        Score.objects.create(user=self.u2, score=15, last_check=datetime.datetime.now())
+
+        self.assertEqual(Score.objects.all().count(), 2)
+
+    def test_incorrect_score(self):
+        Score.objects.create(user=self.u1, score=1.5, last_check=datetime.datetime.now())
+        s = Score.objects.get()
+
+        self.assertEqual(s.score, 1.5)
