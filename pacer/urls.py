c0e1c52bfad83aad1ffba34dc569a0716cd3db9b
Juwaini <juwaini@gmail.com>
2023-02-02 16:34:12 +0800

    forgot import

--- pacer/urls.py
+++ pacer/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from score.views import get_score, get_score_divided
+from score.views import get_score, get_score_divided, set_score
 
 urlpatterns = [
     path('admin/', admin.site.urls),
