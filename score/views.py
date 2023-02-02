8cc02bb24f6bf72925299ae1b64a8774c8b6ad3b
Juwaini <juwaini@gmail.com>
2023-02-02 16:14:33 +0800

    multiply score by 2

--- score/views.py
+++ score/views.py
@@ -6,7 +6,7 @@ from score.models import Score
 def get_score(request, pk):
     try:
         data = Score.objects.get(user=pk)
-        r = {'success': True, 'id': data.user.pk, 'score': data.score}
+        r = {'success': True, 'id': data.user.pk, 'score': data.score * 2}
     except Score.DoesNotExist:
         r = {'success': False, 'message': 'user not found'}
     return JsonResponse(r)
