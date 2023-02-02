c5fee8b79f88fc5ab6a6b8185ccb0998f3e986c9
Juwaini <juwaini@gmail.com>
2023-02-02 16:18:13 +0800

    add another column

--- score/models.py
+++ score/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Score(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     score = models.IntegerField(default=0)
+    score_multiple = models.IntegerField(default=0)
     last_check = models.DateTimeField()
     additional_column = models.CharField(null=True, blank=True, max_length=10)
